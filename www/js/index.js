530ae1eedb10693e79fec71b20fcca55a21d414e
LichuanLu <lulichuan628@gmail.com>
2013-02-04 16:34:58 +0800

    update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -1,62 +1,26 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
-    // Application Constructor
     initialize: function() {
-        this.bindEvents();
+        this.bind();
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
+    bind: function() {
+        document.addEventListener('deviceready', this.deviceready, false);
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        // app.receivedEvent('deviceready');
+    deviceready: function() {
+        alert("ready");
         setTimeout(function() {
-            alert('launching ...');
+            console.log('launching ...');
             window.plugins.childBrowser.showWebPage('http://www.google.com', { showLocationBar: false });
-            alert('launched ...');
+            console.log('launched ...');
         }, 5000);
-    },
-    // Update DOM on a Received Event
-  //   receivedEvent: function(id) {
-  //       // var parentElement = document.getElementById(id);
-  //       // var listeningElement = parentElement.querySelector('.listening');
-  //       // var receivedElement = parentElement.querySelector('.received');
-
-  //       // listeningElement.setAttribute('style', 'display:none;');
-  //       // receivedElement.setAttribute('style', 'display:block;');
-
-  //       console.log('Received Event: ' + id);
-  // //       window.plugins.WifiInfo.get(function(wifi){ 
-  // //       	document.getElementById('wifiText').innerHTML = "<a>WIFI: "+wifi.SSID+"</a>";
-		// //     //alert(wifi.SSID); 
-		// //     document.getElementById('ipText').innerHTML = "<a>IP: "+wifi.IpAddress+"</a>";
-		    
-		// // });
-
-        
-  //   }
+    }
 };
+
+
+$('#open').click = function(){
+     window.plugins.childBrowser.showWebPage('http://www.yahoo.com', { showLocationBar: false });
+
+}
+
+$('#close').click = function(){
+     window.plugins.childBrowser.close();
+}
